7516a0301395af3e9d874dab69097206363d95a6
ghada <ghadabensalem0@gmail.com>
2019-04-03 19:18:34 +0100

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { createOfflineCompileUrlResolver } from '../../node_modules/@angular/compiler';
+import { calcBindingFlags } from '../../node_modules/@angular/core/src/view/util';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  val='';
+  number='';
+  operation='';
+  btnClicked(op){
+    if(op=='')
+        { this.operation='';
+          this.val='';
+        }
+    if(op in['0','1','2',"3","4","5","6","7","8","9"])
+          { this.number=this.number+op;
+           this.val=this.number;
+          // console.log(this.number);
+          }
+    else{
+          this.number='';
+          this.val=op;
+        }
+     this.operation=this.operation+op;
+}
+resultat(){
+  this.val=eval(this.operation);
+  this.operation='';
+  this.number='';
+  }
 }
